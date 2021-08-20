6c4cdb8b57bf6e95df5e099fb9ff0b425d65552e
mecirmartin <mecir.martin@gmail.com>
2021-08-20 15:05:04 +0200

    listen to messages instead of frontend

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import {
   //@ts-ignore
 } from "react-devtools-inline/backend"
 //@ts-ignore
-import { initialize as initializeFrontend } from "./components/frontend"
+import { initialize as initializeFrontend } from "react-devtools-inline/frontend"
+import { Store } from "./bridgeOperations"
 
 import { showDevtoolsWarning } from "./warning"
 
@@ -21,6 +22,17 @@ if (window.__REACT_DEVTOOLS_GLOBAL_HOOK__?.renderers.size === 0) {
   console.log("show warning devtools", window.__REACT_DEVTOOLS_GLOBAL_HOOK__)
   showDevtoolsWarning()
 }
+
+const store = new Store()
+window.addEventListener("message", ({ data }) => {
+  console.log("OPERACIA", data)
+  if (data.event === "operations") {
+    store.onBridgeOperations(data.payload)
+    const devtoolsTree = store._idToElement
+    //@ts-ignore
+    window.devtoolsTree = devtoolsTree
+  }
+})
 
 initializeBackend(window)
 initializeFrontend(window)
